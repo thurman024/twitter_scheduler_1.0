8e33b4e0d104291e0216e84b02b0ca49a6ad1002
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-05-25 11:09:08 -0500

    handle omniauth response

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    @user = User.find_or_create_from_omniauth(request.env['omniauth.auth'])
+    session[:user_id] = @user.id
+    redirect_to root_path
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
 
-  
+  def self.find_or_create_from_omniauth(auth)
+    if user = User.find_by(uid: auth['uid'])
+      user
+    else
+      user = User.create(
+        name: auth.info.nickname,
+        uid: auth.uid,
+        profile_url: auth.info.urls[:Twitter],
+        token: auth.credentials.token,
+        secret: auth.credentials.secret
+      )
+    end
+  end
 end
