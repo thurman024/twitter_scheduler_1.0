0fed01642e93ceff575467da926514ef91e26265
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-05-25 11:29:33 -0500

    add new tweet form and create action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,20 @@
 class TweetsController < ApplicationController
-  def home
+  def new
+    @tweet = Tweet.new
   end
 
+  def create
+    @tweet = Tweet.create(tweet_params)
+    redirect_to tweets_path
+  end
+
+  def index
+    @tweets = Tweet.all
+  end
+
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:content, :time)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'tweets#home'
-
+  root 'tweets#new'
+  resources :tweets
   get '/login', to: 'sessions#new'
   get '/auth/twitter/callback', to: 'sessions#create'
 end
