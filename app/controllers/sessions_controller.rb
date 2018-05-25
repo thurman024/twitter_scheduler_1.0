3bee1b94e101e6eee0d0680e8db11ae50210072b
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-05-24 22:58:55 -0500

    controller name corrected

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
   def new
-
+    @user = User.new
   end
 
   def create
-
+    
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'tweets#home'
+
+  get '/login', to: 'sessions#new'
+  get '/auth/twitter/callback', to: 'sessions#create'
 end
